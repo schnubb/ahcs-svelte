fb62f11d2683313a4212e1c2d817cdf0e75a93a0
Petrakow, Andre (init) <andre.petrakow@init.de>
2020-08-17 21:17:01 +0200

    implement input stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,26 @@
 <script>
 	export let name;
+
+	import { onMount }  from "svelte";
+
+	let inputValue;
+
+	onMount(() => {
+		inputValue = "Test input";
+	});
+
+	const updateValue = event => {
+		inputValue = event.target.value;
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<fieldset>
+		<input type="text" value={inputValue} on:input={updateValue} />
+		<p>{inputValue}</p>
+	</fieldset>
 </main>
 
 <style>
